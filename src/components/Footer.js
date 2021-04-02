a42fab43f5042df5a980bbca167a4f6e0b3d78a0
hubytskyi <hubytskyi@gmail.com>
2021-04-02 13:40:53 +0300

    nothing...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,21 @@ import HubytskyiLogo from "../img/hubytskyi.svg"
 
 const Footer = () => {
 
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-  return (
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="footer__inner">
-              <span>Copyright ©{currentYear} All rights reserved.</span>
-              <span><a href="https://hubytskyi.github.io/hm/" target="_blank" rel="noreferrer"><img src={HubytskyiLogo} alt="hubytskyi"/></a></span>
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="footer__inner">
+                        <span>Copyright ©{currentYear} All rights reserved.</span>
+                        <span><a href="https://hubytskyi.github.io/hm/" target="_blank" rel="noreferrer"><img
+                            src={HubytskyiLogo} alt="hubytskyi"/></a></span>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </footer>
-  )
+        </footer>
+    )
 };
 
 export default Footer
